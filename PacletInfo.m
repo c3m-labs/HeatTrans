c3210c6000f22e1d028365a06a9b596bec480fca
PintarM <matevz.pintar@gmail.com>
2018-11-18 15:00:17 +0100

    Update PacletInfo.m with GitHub homepage

--- PacletInfo.m
+++ PacletInfo.m
@@ -9,8 +9,8 @@ Paclet[
 	Description -> "Package for non-stationary heat transfer simulation.",
 	Creator -> "Matevz Pintar",
 	Publisher -> "C3M d.o.o.",
-	URL->"http://www.c3m.si",
-	Thumbnail->"FrontEnd/icon.png",
+	URL->"https://github.com/c3m-labs/HeatTrans",
+	Thumbnail->"FrontEnd/Logo.png",
 	Extensions -> {
 		{"Kernel",
 			Root -> ".",
